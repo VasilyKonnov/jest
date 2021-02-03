d02e6047ccfc31931be76f20768eab895109150c
VASILY-KONNOV <vasiliy.konnov@gmail.com>
2021-02-03 18:38:31 +0400

    Базовые метчеры и синхронный тест данных

---
+++ 02-sync/sync.js
@@ -0,0 +1,25 @@
+class Lodash {
+	compact(array) {
+		return array.filter(val => !!val)
+	}
+	groupBy(array, prop) {
+
+		return array.reduce((acc, i) => {
+
+			const key = typeof prop === 'function' ? prop(i) : i[prop]
+			console.log('key', key)
+
+			if (!acc[key]) {
+				acc[key] = []
+			}
+
+			acc[key].push(i)
+
+			return acc
+		}, {})
+
+	}
+
+}
+
+module.exports = Lodash
